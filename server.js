f52a856e4c208d3132914492e2741e92b4225be2
Shikong1101 <156152585+Shikong1101@users.noreply.github.com>
2026-05-09 14:00:02 +0800

    fix: 修正 Express 5 路由寫法

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 所有路由都回傳 index.html（SPA 支援）
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
